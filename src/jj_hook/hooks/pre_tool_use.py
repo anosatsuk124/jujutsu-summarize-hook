c84ad11ce989b215b9b3a1dc021e2f9be15de4aa
Satsuki Akiba <anosatsuk124@gmail.com>
2025-07-27 11:05:12 +0900

    Add dependencies to pre_tool_use.py

--- src/jj_hook/hooks/pre_tool_use.py
+++ src/jj_hook/hooks/pre_tool_use.py
@@ -17,10 +17,12 @@ LANGUAGE = os.environ.get("JJ_HOOK_LANGUAGE", "english")
 
 try:
     from ..summarizer import JujutsuSummarizer, SummaryConfig
+    from ..template_loader import load_template
 except ImportError:
     # フォールバック：スクリプトが単体で実行された場合
     warning_msg = "警告: jj_hook パッケージをインポートできませんでした。スタンドアロンモードで実行します。\n" if LANGUAGE == "japanese" else "Warning: Could not import jj_hook package. Running in standalone mode.\n"
     sys.stderr.write(warning_msg)
+    load_template = None
 
 
 def is_jj_repository(cwd: str) -> bool:
